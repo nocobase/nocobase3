9300b4eb90d2da88ab6a7de5ae80e29a4601b9b3
gchust <gchust@qq.com>
2026-08-27 09:15:13 +0800

    chore: clean code

--- packages/app-plugin-files/database/migrations/202608221000_files_create_files.ts
+++ packages/app-plugin-files/database/migrations/202608221000_files_create_files.ts
@@ -1,12 +1,28 @@
 import { defineMigration } from '@nocobase/app-database';
 
-import { defineFilesCollection } from '../files-collection.js';
-
 export default defineMigration({
   name: '202608221000_files_create_files',
 
   async up({ builder }) {
-    await builder.createCollection('files', defineFilesCollection);
+    await builder.createCollection('files', (collection) => {
+      collection.string('id', { length: 64 }).notNull();
+      collection.string('status', { length: 16 }).notNull();
+      collection.string('storageKey', { length: 512 }).nullable();
+      collection.string('name', { length: 255 }).notNull();
+      collection.bigInt('size').nullable();
+      collection.string('contentType', { length: 255 }).nullable();
+      collection.datetime('uploadExpiresAt').notNull();
+      collection.string('publicTokenHash', { length: 512 }).nullable();
+      collection.string('publicDisposition', { length: 16 }).nullable();
+      collection.datetime('createdAt').notNull();
+      collection.datetime('updatedAt').notNull();
+
+      collection.primary('id', { name: 'pk_files' });
+      collection.unique('storageKey', { name: 'uq_files_storage_key' });
+      collection.index(['status', 'uploadExpiresAt'], {
+        name: 'idx_files_status_upload_expires_at',
+      });
+    });
   },
 
   async down({ builder }) {
