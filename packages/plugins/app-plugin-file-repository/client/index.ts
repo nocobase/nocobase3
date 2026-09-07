f17d3f6a7e1ba60ecd8a4879700d7eae77365b33
chenos <chenlinxh@gmail.com>
2026-09-07 19:29:38 +0800

    feat(file-repository): add file services, routes and attachments example

---
+++ packages/plugins/app-plugin-file-repository/client/index.ts
@@ -0,0 +1,5 @@
+export { default } from './plugin.js';
+export { ClientFileRepositoryManager } from './manager.js';
+export type { ClientFileRepository } from './manager.js';
+export { clientFileRepositoryManagerToken } from './token.js';
+export type * from '../shared/types.js';
