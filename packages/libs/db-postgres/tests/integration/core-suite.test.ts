d4b836adfc61bd9b85dc390cf5e07d43ef95b3de
chenos <chenlinxh@gmail.com>
2026-09-11 07:03:21 +0800

    test(db-testkit): expose stable integration suite entrypoint

--- packages/libs/db-postgres/tests/integration/core-suite.test.ts
+++ packages/libs/db-postgres/tests/integration/core-suite.test.ts
@@ -1,27 +1,5 @@
 import { installDatabaseIntegrationAdapter } from '@nocobase/db-testkit';
 import { postgresDialectIntegrationAdapter } from './legacy-adapter.js';
 
-declare global {
-  interface ImportMeta {
-    glob(
-      pattern: string,
-      options?: { eager?: boolean },
-    ): Record<string, () => Promise<unknown>>;
-  }
-}
-
 installDatabaseIntegrationAdapter(postgresDialectIntegrationAdapter);
-process.chdir(new URL('../../../db-testkit/', import.meta.url).pathname);
-
-const loadTests = import.meta.glob(
-  '../../../db-testkit/tests/integration/**/*.test.ts',
-  {
-    eager: false,
-  },
-);
-
-for (const loadTest of Object.values(loadTests) as Array<
-  () => Promise<unknown>
->) {
-  await loadTest();
-}
+await import('@nocobase/db-testkit/integration-suite');
